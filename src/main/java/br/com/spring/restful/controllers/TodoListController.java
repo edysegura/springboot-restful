0c5366d9b522bee24c2032abb68a4acf07d1046b
Edy Segura <edysegura@gmail.com>
2020-05-13 12:18:57 -0300

    Add CRUD operations

--- src/main/java/br/com/spring/restful/controllers/TodoListController.java
+++ src/main/java/br/com/spring/restful/controllers/TodoListController.java
@@ -1,22 +1,26 @@
 package br.com.spring.restful.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.spring.restful.models.Todo;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/tasks")
 public class TodoListController {
 
-  @GetMapping(value = "/tasks")
+  @GetMapping()
   public String findAll() {
-    return "Hi there!";
+    return "Hi there! Tasks will be listed here soon!";
   }
 
-  @GetMapping(value = "/tasks/{id}")
+  @GetMapping(value = "/{id}")
   public Todo getById(@PathVariable(value = "id") Long id) {
     Todo todo = new Todo();
     todo.setId(id);
@@ -24,4 +28,20 @@ public class TodoListController {
     return todo;
   }
 
+  @PostMapping()
+  public Todo create(@RequestBody Todo entity) {
+    entity.setId(32L);
+    return entity;
+  }
+
+  @PatchMapping(value = "/{id}")
+  public Todo update(@PathVariable Long id, @RequestBody Todo entity) {
+    entity.setId(id);
+    return entity;
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public String delete(@PathVariable Long id) {
+    return "Ok";
+  }
 }
